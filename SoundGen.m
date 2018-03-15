22f17eeea62b4b2b70afbfb37046acdc1873548b
Diogo Batista <diogo_speedragon_batista@hotmail.com>
2018-03-15 16:38:44 +0000

    Commit

--- SoundGen.m
+++ SoundGen.m
@@ -15,12 +15,10 @@ freq_beep=500;
 values=0:1/fs_beep:duration;
 a=amp*sin(2*pi*freq_beep*values);
 sound(a,fs_beep)
-flag = 0;
 
 %% ===========================================================================
 %% SDR RTL HARDWARE SETUP
 %% ===========================================================================
-
 
 fmRxParams = getParamsSdrrFMExamples;
 
@@ -50,7 +48,6 @@ radio = comm.SDRRTLReceiver('CenterFrequency', Fc, ...
 signal_frequency = 3650; % <------ frequency of the unmodulated signal sent by the sensor
 bits_per_word = 11; % <----- has to be adjusted to the expected recieved transmission
 
-
 signal_period = 1/signal_frequency; % <----- period of the unmodulated signal
 samples_per_bit = Fs * signal_period; % <----- number of samples in each bit of the recieved message, this number is not necessarily an integer
 bits_per_frame = SPF/samples_per_bit;
@@ -64,13 +61,10 @@ unmodulated_signal = zeros(1000, 1);  % <----- Demodulated signal gets stored he
 f = 0;  % <----- total frames processed counter;
 data = [];
 alldata = [];
-endresult = [];
-allvariance = [];
-allword_frontier = [];
-alloffsets = 0;
 
 m = 6; % <--- it's advisable to select m such as bilmf > 1000 to facilitate the UI generation. under normal circunstances this means m >= 6
 bilmf = zeros(ceil(bits_per_frame * m), 1);  % <-------- Bits in Last m Frames: stores last 3 frames worth of bits for pattern recognition
+
 
 % WORD DETECTION VARIABLES
 
@@ -80,13 +74,12 @@ word_map = [];
 word_frontier = [];
 number_of_slices = 0;
 
-envelope_function = zeros(SPF, 1);
+envelope_function = zeros(SPF, 1);   % < -- pre allocate space for the envelope function
 
 first_frontier = 1;
 final_frontier = 1;
 
 initial_frame_discard = 5; % <  ---- specifies the number of disregarded initial frames at program startup, improves envelope stabilization and ultimately the sucess rate
-rising_edge_counter = 0;
 word_success_rate = 0; % <------ hits/tries (NOT 100% TRUSTABLE);
 bit_success_rate = 0; % <------ nthroot(bits_per_word, word_success_rate)
 
@@ -96,12 +89,13 @@ envelope_offset = 0; % <------ envelope line control
 
 frame_start = zeros(1000,1);
 
-desired_result = transpose([1 1 0 1 0 1 0 1 0 1]);
+desired_result = transpose([1 1 0 1 0 0 0 1 0 0 0 1 0 1 0 0 1 1 0 0 0 1 1 1 0 0 0 0 1 1 1 1]);
 reset_pattern = transpose([0 0 0 0 0 0 0 0 0 1]);
 rising_edge = transpose([0 1]);
 patterns = {desired_result; reset_pattern; rising_edge};
 counter_patterns = zeros(1, length(patterns));  % <---- counts how many patterns are detected in the signal    MAP = [hits, tries]
 
+message = [,]; % < ------ first column for the data, second one for the hexadecimal translation
 
 %% ===========================================================================
 %% GUI SETUP
@@ -113,17 +107,8 @@ Interface.GUI_display_fc(num2str(Fc));
 debug_mode = 1; % <- binary variable that determines if permanent samples are stored for later debugging. WARNING: PROGRAM WILL SHARPLY DECREASE IN PERFORMANCE AFTER THE FIRST 20 SECONDS WHILE IN THIS MODE
 
 %% ===========================================================================
-%% MULTITHREADING (Gave up on this, unessecary and impossible to implement)
-%% ===========================================================================
-
-  %parallel_worker = batch('parallel_plotting');
-  %parallel_worker_ID =
-
-%% ===========================================================================
 %% MAIN CYCLE
 %% ===========================================================================
-
-%pause(2);
 
 tic;
 while radioTime < fmRxParams.StopTime
@@ -149,21 +134,13 @@ while radioTime < fmRxParams.StopTime
 
       % HORIZONTAL FRAMING (bit_frontier, word_window)
 
-      window_variance = zeros(SPF, 1);
-
-      for a0 = 1 : SPF - length(word_window)
-        %window_stop = a0+length(word_window)-1;
-        word_window = frame_absolute(a0:a0+length(word_window)-1);
-        window_variance(a0, 1) = var(word_window);
-      end
-      window_variance(a0+1:end) = window_variance(a0);
+      window_variance = define_windowvariance(SPF, frame_absolute, length(word_window));
 
       split = max(window_variance)*0.40;
       word_map = im2bw(window_variance, split);
       word_frontier = [(abs(word_map(1:end-1)-word_map(2:end))); 0];  % <------ these three lines create an array that is filled with zeros except when a word begins or ends
       word_frontier(round(samples_per_word/1.66)+1 : end) = word_frontier(1:end-round(samples_per_word/1.66));
       word_frontier(1:round(samples_per_word/1.66)) = 0;
-
 
       number_of_slices = nnz(word_frontier);
 
@@ -187,30 +164,16 @@ while radioTime < fmRxParams.StopTime
         slice_index = find(word_frontier, number_of_slices);  % returns the index of the first n ones in the variable word_frontier, where n = number of slices
         slice_index = [1; slice_index; SPF];
 
-
         for a1 = 1 : length(slice_index)-1;
-          data{a1, 1} = frame_absolute(slice_index(a1):slice_index(a1+1)-1);    % < -- save the signal itself in the first column of the cell array
-          data{a1, 3} = threshold(slice_index(a1):slice_index(a1+1)-1);
-          data{a1, 6} = envelope_function(slice_index(a1):slice_index(a1+1)-1, 1:2);
-          data{a1, 7} = window_variance(slice_index(a1):slice_index(a1+1)-1);
-          data{a1, 8} = word_frontier(slice_index(a1):slice_index(a1+1)-1);
+          data{a1, 1} = frame_absolute(slice_index(a1):slice_index(a1+1)-1);            % < -- save the signal itself in the first column of the cell array
+          data{a1, 3} = threshold(slice_index(a1):slice_index(a1+1)-1);                 % < -- threshold levels
+          data{a1, 6} = envelope_function(slice_index(a1):slice_index(a1+1)-1, 1:2);    % < -- envelope levels, both upper and lower
+          data{a1, 7} = window_variance(slice_index(a1):slice_index(a1+1)-1);           % < -- variance of the signal in a given interval (defined by length(word_window))
+          data{a1, 8} = word_frontier(slice_index(a1):slice_index(a1+1)-1);             % < -- map of the places where a word begins or ends
 
           [~, slice_offset] = Synchronize(data{a1, 1}, samples_per_bit , data{a1, 3});   % <--- save the corresponding bit frontiers for that slice and word in the respective row of the second column
 
-          slice_bit_frontiers =  zeros(floor(length(data{a1, 1})/samples_per_bit), 1);
-          alloffsets(length(alloffsets)+1) = slice_offset;
-
-          for a2 = 1 : length(slice_bit_frontiers)
-            slice_bit_frontiers(a2) = slice_offset + round((a2-1) * samples_per_bit);
-          end
-
-          slice_bit_frontiers = [slice_bit_frontiers; length(data{a1, 1})];
-
-          if  ~isempty(slice_bit_frontiers)
-            if slice_bit_frontiers(1) ~= 1
-              slice_bit_frontiers = [1; slice_bit_frontiers]; % <- include 0 and end on the frontiers, this means the first and last interval will be uneven.
-            end
-          end
+          slice_bit_frontiers = define_bitfrontiers(length(data{a1, 1}), slice_offset, samples_per_bit);
 
           data{a1, 2} = slice_bit_frontiers;
 
@@ -221,35 +184,30 @@ while radioTime < fmRxParams.StopTime
         for a3 = 1 : nrData
 
           data{a3, 5} = intervals_average(data{a3, 1}, data{a3, 2});
-          data{a3, 4} = ASK_Demod(data{a3, 1}, (data{a3, 3} .* thresh_gain) + thresh_offset, data{a3, 2});
+          data{a3, 4} = ASK_Demod(data{a3, 1}, (data{a3, 3} .* thresh_gain) + thresh_offset, data{a3, 2});      % < -- demodulation of the signal
 
           for a4 = 1 : length(data{a3, 4})  % <------- detect patterns, bit by bit
-            bilmf(1:end-1) = bilmf(2:end);  % <---- Será vantajoso fazer isto de um a um ou seria melhor passar este ciclo para dentro de uma funçao?
+            bilmf(1:end-1) = bilmf(2:end);
             bilmf(end) = data{a3, 4}(a4);
 
-            if f > initial_frame_discard + n/3 + 10% <---- the program has been running AND processing data for a while before it starts actively trying to identify patters, provides more accurate pattern recognition output data
-              counter_patterns =  counter_patterns + FindPattern(patterns, bilmf);
+            if f > initial_frame_discard + n/3 + 10   % <---- the program has been running AND processing data for a while before it starts actively trying to identify patters, provides more accurate pattern recognition output data
+              patterns_buffer = FindPattern(patterns, bilmf);
+              counter_patterns =  counter_patterns + patterns_buffer;
+
+              if patterns_buffer(1,1) == 1
+                window1 = length(bilmf)-length(desired_result)+2;
+                window2 = length(bilmf)-length(desired_result)+10;
+                message(counter_patterns(1)*2) = bilmf(window1:window1+3);
+                message(counter_patterns(1)*2+1) = bilmf(window1+4:window1+7);
+              end
+
             end
           end
         end
       end
 
       if debug_mode == 1
-
         alldata = cat (1, alldata, data);
-
-        for a7 = 1 : length(data)
-
-          %allthreshold = cat(1, allthreshold, data{a7, 3});
-          %endresult = cat(1, endresult, data{a7, 4});
-
-        end
-        %allsamples = cat(1, allsamples, frame_absolute);
-        allword_frontier = cat(1, allword_frontier, word_frontier);
-        %allenvelope = cat(1, allenvelope, envelope_function);
-        %allvariance = cat(1, allvariance, window_variance);
-        %allthreshold = cat(1, allthreshold, threshold);
-        %allbit_frontier = cat(1, allbit_frontier, (data{a7, 2} + ((f-1)*SPF) ) );
       end
 
       word_success_rate = (counter_patterns(1, 1)/counter_patterns(1, 2));
@@ -260,18 +218,13 @@ while radioTime < fmRxParams.StopTime
 
       pause(1e-8);
 
-
-      %=================================
+      %====================================================
       % Custom functions go here
-      %=================================
-
-      %lost = 0;
-      %late = 1;
+      %====================================================
 
     end
 
   end
-
 
   Interface.GUI_plot(bilmf);
   Interface.GUI_RE_counter(num2str(counter_patterns(3)));
@@ -298,8 +251,8 @@ end
 release(radio);
 delete(Interface);
 
-disp(word_success_rate);
-disp(bit_success_rate);
+word_success_rate
+bit_success_rate
 
 %% ===========================================================================
 %% FINAL CHARTS
